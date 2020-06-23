5e75592a6db6f2513c2ccfc6e7a4ffda82ef8c98
Srikrishna <srike27@gmail.com>
2020-06-23 20:26:37 +0530

    updated constants and printed some things

--- sbrobot_control/src/pid_balance.py
+++ sbrobot_control/src/pid_balance.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 import rospy
 from sensor_msgs.msg import Imu
+from sensor_msgs.msg import JointState
 from std_msgs.msg import Float64
 import math as m
 
 class controller:
     def __init__(self):
-        rospy.Subscriber("imu", Imu, self.callback)
+        rospy.Subscriber("imu", Imu, self.icallback)
+        rospy.Subscriber("joint_states", JointState, self.jcallback)
         self.lspdpub = rospy.Publisher("left_motor_controller/command",Float64,queue_size = 20)
         self.rspdpub = rospy.Publisher("right_motor_controller/command",Float64,queue_size = 20)
         self.ax = 0.0
@@ -16,8 +18,10 @@ class controller:
         self.aydot = 0.0
         self.azdot = 0.0
         self.sumay = 0.0
+        self.lvm = 0.0
+        self.rvm = 0.0
 
-    def callback(self,msg):
+    def icallback(self,msg):
         x = msg.orientation.x
         y = msg.orientation.y
         z = msg.orientation.z
@@ -26,23 +30,31 @@ class controller:
         self.ax = ang*(x)/m.sqrt(1-w*w)
         self.ay = ang*(y)/m.sqrt(1-w*w)
         self.az = ang*(z)/m.sqrt(1-w*w)
-        if(self.ay > 0.0001 and self.ay < -0.0001):
-            self.sumay += self.ay/50
-        else:
-            self.sumay = 0.0
+        self.sumay += self.ay/50.0
         self.axdot = msg.angular_velocity.x
         self.aydot = msg.angular_velocity.y
         self.azdot = msg.angular_velocity.z
+
+    def jcallback(self,msg):
+        i = 0
+        for j in msg.velocity:
+            if i == 0:
+                self.lvm = j
+            if i == 1:
+                self.rvm = j
+            i += 1
+        
         
     def listener(self):
         rate = rospy.Rate(50)
-        kp = 300.0
-        kd = 10.0
-        ki = 3.0
+        kp = 100.0
+        kd = 0.0
+        ki = 300.0
         while(True):
             spd = kp*(self.ay) + kd*(self.aydot) + ki*(self.sumay)
             ls = Float64()
             rs = Float64()
+            print(spd,self.lvm,self.rvm)
             ls.data = spd
             rs.data = spd
             self.lspdpub.publish(ls)
