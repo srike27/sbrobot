5e47d4a6aa1d75cb386878707c87448c9cfe78e4
Srikrishna <srike27@gmail.com>
2020-06-24 20:53:08 +0530

    subsribing to setpoint and diffvel

--- sbrobot_control/src/pid_balance.py
+++ sbrobot_control/src/pid_balance.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import rospy
 from sensor_msgs.msg import Imu
+from sbrobot_control.msg import control
 from sensor_msgs.msg import JointState
 from std_msgs.msg import Float64
 import math as m
@@ -9,28 +10,35 @@ class controller:
     def __init__(self):
         rospy.Subscriber("imu", Imu, self.icallback)
         rospy.Subscriber("joint_states", JointState, self.jcallback)
+        rospy.Subscriber("control", control, self.ccallback)
         self.lspdpub = rospy.Publisher("left_motor_controller/command",Float64,queue_size = 20)
         self.rspdpub = rospy.Publisher("right_motor_controller/command",Float64,queue_size = 20)
         self.ax = 0.0
         self.ay = 0.0
         self.az = 0.0
+        self.aw = 0.0
         self.axdot = 0.0
         self.aydot = 0.0
         self.azdot = 0.0
-        self.sumay = 0.0
+        self.sumerr = 0.0
         self.lvm = 0.0
         self.rvm = 0.0
+        self.err = 0.0
+        self.setpoint = 0.0
+        self.diffvel = 0.0
+
+    def ccallback(self,msg):
+        self.setpoint = msg.set_point
+        self.diffvel = msg.diff_vel
 
     def icallback(self,msg):
         x = msg.orientation.x
         y = msg.orientation.y
         z = msg.orientation.z
         w = msg.orientation.w
-        ang = 2*m.acos(w)
-        self.ax = ang*(x)/m.sqrt(1-w*w)
-        self.ay = ang*(y)/m.sqrt(1-w*w)
-        self.az = ang*(z)/m.sqrt(1-w*w)
-        self.sumay += self.ay/50.0
+        self.ay = m.asin(2*(w*y - z*x))
+        self.err = self.ay - self.setpoint
+        self.sumerr += self.err/50.0
         self.axdot = msg.angular_velocity.x
         self.aydot = msg.angular_velocity.y
         self.azdot = msg.angular_velocity.z
@@ -51,18 +59,18 @@ class controller:
         kd = 0.0
         ki = 300.0
         while(True):
-            spd = kp*(self.ay) + kd*(self.aydot) + ki*(self.sumay)
+            spd = kp*(self.err) + kd*(self.aydot) + ki*(self.sumerr)
             ls = Float64()
             rs = Float64()
-            print(spd,self.lvm,self.rvm)
-            ls.data = spd
-            rs.data = spd
+            print(self.setpoint,self.ay)
+            ls.data = spd - self.diffvel
+            rs.data = spd + self.diffvel
             self.lspdpub.publish(ls)
             self.rspdpub.publish(rs)
             rate.sleep()
 
 if __name__ == '__main__':
-    rospy.init_node('pidcontroller', anonymous=True)
+    rospy.init_node('pid_balance_controller', anonymous=True)
     a = controller()
     try:
         a.listener()
